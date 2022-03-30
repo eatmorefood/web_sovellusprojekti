5593dd7f89ed0c8784af284c9ac62493faa7022f
moilanenantti <antti.moilanen@hotmail.fi>
2022-03-30 15:07:19 +0300

    fixed bugs in login and signup

---
+++ nodejs/config/passport.js
@@ -0,0 +1,83 @@
+const bcrypt= require('bcryptjs');
+const res = require('express/lib/response');
+const pool = require('../database.js')
+
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+    passport.deserializeUser(function(user, done) {
+        done(null, user);
+    });
+    passport.use(new BasicStrategy(
+    function(email, password, done){
+        loginUser();
+
+    async function loginUser() {
+        const client = await pool.connect();
+        try {
+            await client.query('begin');
+            var accCredentials = await JSON.stringify(client.query('select c."idcustomer" as idcustomer, u."password" as password from "customers" c, "userc" u where c."email" = ($1) and c."idcustomer" = u."idcustomer";',
+            [email], (err, result) => {
+                if(err){
+                    res.status(500);
+                    return done(err);
+                }
+                if(result.rows[0] == null) {
+                    //console.error('incorrect email');
+                    res.status(401);
+                    return done(null, false);
+                } else {
+                    //console.log(result.rows[0])
+                    //console.log("user credentials found")
+
+                    bcrypt.compare(password, result.rows[0].password, (err, valid) => {
+                        if(err) {
+                            console.error('password validation failed');
+                            res.status(500);
+                            return done(err);
+                        }
+                        if(valid) {
+                            //console.log('password correct');
+
+                            var accData = JSON.stringify(client.query('select "idcustomer", "fname", "lname", "email" from "customers" where "idcustomer" = $1;',
+                            [result.rows[0].idcustomer], (err, results) => {
+                                if(err){
+                                    res.status(500);
+                                    return done(err);
+                                }
+                                if(results.rows[0] == null) {
+                                    console.error('could not retrieve user data after valid login');
+                                    res.status(400);
+                                    return done(null, false);
+                                } else {
+                                    var user = {
+                                        id: results.rows[0].idcustomer,
+                                        email: results.rows[0].email,
+                                        fname: results.rows[0].fname,
+                                        lname: results.rows[0].lname
+                                    }
+                                    //console.log(results);
+                                    console.log("user with email: " + user.email + " has logged in");
+                                    res.status(200);
+                                    done(null, user);
+                                }
+                            }));
+                        } else {
+                            //console.log('incorrect password');
+                            res.status(401);
+                            return done(null, false);
+                        }
+                    })
+                }
+            }))
+        } catch(e){
+            res.status(500);
+            throw(e);            
+        }
+    }
+  }));
+
+}
--- nodejs/models/login_model.js
+++ nodejs/models/login_model.js
@@ -3,18 +3,21 @@ const db = require('../database');
 const login={
 
     findId: function(email, callback) {
-        return db.query('select idcustomer from customers where email = $1;',
+      await db.connect();
+        return await db.query('select idcustomer from customers where email = $1;',
         [email], callback); 
-      },
+    },
 
     checkPassword: function(userId, callback) {
-        return db.query('select password from userc where idcustomer = $1;',
-        [userId], callback); 
-      },
+      await db.connect();
+      return await db.query('select password from userc where idcustomer = $1;',
+      [userId], callback); 
+    },
 
     getData: function(userId, callback) {
-    return db.query('select idcustomer, fname, lname, email from customers where idcustomer = $1;',
-    [userId], callback); 
+      await db.connect();
+      return  db.query('select idcustomer, fname, lname, email from customers where idcustomer = $1;',
+      [userId], callback); 
     }
   };
             
