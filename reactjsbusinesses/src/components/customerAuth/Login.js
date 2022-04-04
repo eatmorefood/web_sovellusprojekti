f6fb2aa164687ed6f473165f02f2c29ae2dea435
EliasPir <elias.pirttikangas@gmail.com>
2022-04-05 00:24:10 +0300

    Set up business side and sign up for businesses still in development

---
+++ reactjsbusinesses/src/components/customerAuth/Login.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './Login.css';
+import axios from 'axios';
+import closeBtn from '../../images/closeBtnCircle.png';
+import Constants from '../../Constants.json';
+import { useNavigate } from "react-router-dom";
+
+function Login( props ){ 
+  let navigate = useNavigate();
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await axios.post(Constants.API_ADDRESS + "/jwtLogin",
+      null,
+      {
+        auth: {
+          username: event.target.email.value,
+          password: event.target.password.value
+        }
+      });
+      if(window.location.pathname.indexOf("signup") > -1){
+        navigate('/');
+      }
+      const receivedJWT = result.data.jwt;
+      props.login(receivedJWT);
+
+    } catch(error) {
+      var body = document.getElementById("loginErrorMessage");
+      var x = document.getElementById("loginEmail");      
+      var y = document.getElementById("loginPassword");
+      body.style.visibility = "visible";
+      x.style.borderColor = "red";
+      x.value = "";
+      y.style.borderColor = "red";
+      y.value = "";
+      setTimeout(function() {
+        body.style.visibility = "hidden";
+        x.style.borderColor = "lightgrey";
+        y.style.borderColor = "lightgrey";
+      }, 5000);
+    }
+    
+  }
+
+  return (
+    <div className="Login animateLoginZoom">
+      <img className="closeLogin" src={closeBtn} onClick={ props.displayLogin } alt="x" />
+      <div className="loginContent">
+        <div className="loginTitle">Log in or create account</div>
+        <div className="loginSubTitle">Log in below or create a new EatMoreFood account.</div>
+        <div id="loginErrorMessage">Wrong email or password!</div>
+        <form className="loginForm" onSubmit={ handleLoginSubmit }>
+          <input
+            type="email"
+            id="loginEmail"
+            name="email"
+            placeholder="email"
+            autoComplete="off"
+            required
+          />
+          <input
+            type="password"
+            id="loginPassword"
+            name="password"
+            placeholder="password"
+            autoComplete="off"
+            required
+          />
+          <input
+            type="submit"
+            className="loginSubmit"
+            value="Log in"
+          />
+          <div className="loginMiddleText">OR</div>
+          <button className="loginBtnRegister" onClick={(e) => { e.preventDefault(); window.location.href='/signup'; }}>Sign up</button>
+      </form>  
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ reactjsbusinesses/src/components/signupComponents/UserDetails.js
@@ -0,0 +1,97 @@
+import React, { useEffect } from 'react'
+import '../customerAuth/Signup.css';
+
+//This is part 1/4 of the multiphase signup form
+const UserDetails = ({ nextStep, handleChange, values }) => {
+
+    useEffect(() => { //automatically scrolls to the top of the page, useful for mobile users
+        window.scrollTo(0, 0)
+      }, []);
+
+  return (
+    <div className="Signup">
+        <div className="signupContent">
+            <div className="signupTitle">Hello! Nice to meet you &#128516;</div>
+            <div className="signupSubTitle">Create a new EatMoreFood account for a business below. <br/>After the process your information will be automatically checked.</div>
+            <form className="signupForm" onSubmit={ nextStep }>
+                <div className="signupInputTitle">Email:</div>
+                <input
+                    type="email"
+                    className="signupField"
+                    name="email"
+                    placeholder="email"
+                    onChange={handleChange('email')}
+                    defaultValue={values.email}
+                    autoComplete="off"
+                    maxLength="50"
+                    required
+                />
+                <div className="signupInputTitle">Restaurant name:</div>
+                <input
+                    type="text"
+                    className="signupField"
+                    name="name"
+                    placeholder="restaurant name"
+                    onChange={handleChange('name')}
+                    defaultValue={values.name}
+                    autoComplete="off"
+                    maxLength="40"
+                    required
+                />
+                <div className="signupInputTitle">Address:</div>
+                <input
+                    type="text"
+                    className="signupField"
+                    name="address"
+                    placeholder="street address"
+                    onChange={handleChange('address')}
+                    defaultValue={values.address}
+                    autoComplete="off"
+                    maxLength="35"
+                    required
+                />
+                <div className="signupInputTitle">Open:</div>
+                <input
+                    type="text"
+                    className="signupField"
+                    name="open"
+                    placeholder="open"
+                    onChange={handleChange('open')}
+                    defaultValue={values.open}
+                    autoComplete="off"
+                    maxLength="5"
+                    required
+                />
+                <div className="signupInputTitle">Type:</div>
+                <input
+                    type="text"
+                    className="signupField"
+                    name="type"
+                    placeholder="type"
+                    onChange={handleChange('type')}
+                    defaultValue={values.type}
+                    autoComplete="off"
+                    maxLength="35"
+                    required
+                />
+                <div className="signupInputTitle">Price level:</div>
+                <input
+                    type="text"
+                    className="signupField"
+                    name="pricelevel"
+                    placeholder="price level"
+                    onChange={handleChange('pricelevel')}
+                    defaultValue={values.pricelevel}
+                    autoComplete="off"
+                    maxLength="35"
+                    required
+                />
+                <button value="submit" className="multiphaseFormBtn">Next</button>
+            </form>
+
+        </div>
+    </div>
+  )
+}
+
+export default UserDetails;
