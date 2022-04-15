aa7cf0a4e21da0d55b67d0e867e8b5c6ea4d3259
Karahi1 <t0raka02@students.oamk.fi>
2022-04-15 15:57:22 +0300

    Allrestaurants UI muokkausta

--- reactjs/src/components/discover/DiscoverAll.js
+++ reactjs/src/components/discover/DiscoverAll.js
@@ -1,26 +1,42 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './DiscoverAll.css';
 
 
 function DiscoverAll(props) {
 
+    const navigate = useNavigate();
+
+    function RestaurantClicked(params) { //direct to restaurant page when user clicks search result
+        const restaurantPath = params.idrestaurant;
+        navigate(`/restaurant/${restaurantPath}`);
+    }
+
+
     return (  
+        
         <div className="restaurantContainer">
         {
       
           <>{props.allRestaurants.map((item, index) => {
               return (
                   
-                    <div className="restaurant">
+                    <div id="restaurant" onClick={() => RestaurantClicked(item)}>
+                        <div className="restaurant">                        
                             <div key={index}></div>
-                            <img src={ item.image }></img>
-                            <div>{item.name}</div>
-                            <div>{item.type}</div>
-                            <div>{item.pricelevel}</div>
+                            <img src={ item.image } alt="" loading="eager"></img>
+                            <div className="restaurantInnerContainer">
+                                <div className="restaurantName">{item.name}</div>
+                                <div className="restaurantType">{item.type}</div>
+                            </div>
+                            <div className="restaurantsDivider" />
+                            <div className="restaurantsInnerBottomContainer">
+                                <div>{item.pricelevel}</div>
+                                <div>·</div>
+                                <div>{item.address}</div>
+                            </div>
                         </div>
-                   
-                
-        
+                    </div>    
               
             )
             })}</> 
