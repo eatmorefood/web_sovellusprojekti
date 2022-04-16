ff36bc0448d618e36a58c268f2dfe14b070c1acd
Karahi1 <t0raka02@students.oamk.fi>
2022-04-16 16:38:42 +0300

    Allrestaurants-path ready

--- reactjs/src/components/discover/DiscoverAll.js
+++ reactjs/src/components/discover/DiscoverAll.js
@@ -14,34 +14,41 @@ function DiscoverAll(props) {
 
 
     return (  
+
+       <div className="DMain">
+           <h1>All restaurants</h1>
+            <div className="DrestaurantContainer">
+                
+            {
         
-        <div className="restaurantContainer">
-        {
-      
-          <>{props.allRestaurants.map((item, index) => {
-              return (
-                  
-                    <div id="restaurant" onClick={() => RestaurantClicked(item)}>
-                        <div className="restaurant">                        
-                            <div key={index}></div>
-                            <img src={ item.image } alt="" loading="eager"></img>
-                            <div className="restaurantInnerContainer">
-                                <div className="restaurantName">{item.name}</div>
-                                <div className="restaurantType">{item.type}</div>
+            <>{props.allRestaurants.map((item, index) => {
+                return (
+                    
+                    
+                        <div id="DrestaurantID" onClick={() => RestaurantClicked(item)}>
+                            <div className="Drestaurant">                        
+                                <div key={index}></div>
+                                <img src={ item.image } alt="" loading="eager"></img>
+                                <div className="DrestaurantInnerContainer">
+                                    <div className="DrestaurantName">{item.name}</div>
+                                    <div className="DrestaurantType">{item.type}</div>
+                                </div>
+                                <div className="DrestaurantsDivider" />
+                                <div className="DrestaurantsInnerBottomContainer">
+                                    <div>{item.pricelevel}</div>
+                                    <div>·</div>
+                                    <div>{item.address}</div>
+                                </div>
                             </div>
-                            <div className="restaurantsDivider" />
-                            <div className="restaurantsInnerBottomContainer">
-                                <div>{item.pricelevel}</div>
-                                <div>·</div>
-                                <div>{item.address}</div>
-                            </div>
-                        </div>
-                    </div>    
-              
-            )
-            })}</> 
-        }  
+                        </div>   
+                    
+                
+                )
+                })}</> 
+            }  
+            </div>
         </div>
+        
     );
 }
 
