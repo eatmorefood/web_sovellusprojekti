59ffca53979068cc4dcfe86d87683c3112eb0ee6
Antti Moilanen <91624045+moilanenantti@users.noreply.github.com>
2022-04-18 23:00:40 +0300

    Update signupbusiness_model.js

--- nodejs/models/signupbusiness_model.js
+++ nodejs/models/signupbusiness_model.js
@@ -3,7 +3,7 @@ const db = require('../database');
 const business={
 
     add: function(newBusiness, callback) {
-          return db.query('with new_business as (insert into restaurant (name, address, open, type, pricelevel, image) values ($1, $2, $3, $4, $5, $8) returning idrestaurant as insertedrestaurantid) insert into userr (password, email, idrestaurant) select $6, $7, insertedrestaurantid from new_business;',
+          return db.query('with new_business as (insert into restaurant (name, address, open, type, pricelevel, image) values ($1, $2, $3, $4, $5, $8) returning idrestaurant as insertedrestaurantid) insert into userr (password, email, idrestaurant) select $6, $7, insertedrestaurantid from new_business returning idrestaurant;',
           [newBusiness.name, newBusiness.address, newBusiness.open, newBusiness.type, newBusiness.pricelevel, newBusiness.password, newBusiness.email, newBusiness.image], callback);
       },
     getProfileData: function(ID, callback) { 
